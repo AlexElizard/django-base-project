24f9e740ef727f0d54f7c273586c6bfc58d15bb6
AlexElizard <alexelizard@gmail.com>
2021-11-10 14:27:27 +1000

    Добавлена функция проверки uuid

--- services/utils.py
+++ services/utils.py
@@ -1,3 +1,16 @@
+import uuid
+
+
+def is_valid_uuid(value: str, version: int = 4) -> bool:
+    """Проверяет является ли value валидным uuid"""
+    try:
+        uuid.UUID(value, version=version)
+    except ValueError:
+        return False
+    else:
+        return True
+
+
 def progress_bar(current, total, title="Please wait", bar_len=50):
     """Прогресс бар для терминала"""
     percent_done = round(100 * (current + 1) / total, 1)
